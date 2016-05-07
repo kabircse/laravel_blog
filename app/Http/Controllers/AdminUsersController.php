1ff1de79d42ad372a11852a8d84ea3f556823725
Kabir <kabir.pust@gmail.com>
2016-05-07 18:31:22 +0600

    :smile: added files to first step

---
+++ app/Http/Controllers/AdminUsersController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests\UsersRequest;
+use App\User;
+use App\Role;
+use App\Photo;
+
+class AdminUsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('admin.users.index',compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $roles = Role::lists('name','id')->all();
+        return view('admin.users.create',compact('roles'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UsersRequest $request)
+    {
+        $inputs = $request->all();
+        if($file = $request->file('photo')){
+            $name = date('Y-m-d_his_').$file->getClientOriginalName();
+            $file->move('uploads/images/profile_picture',$name);
+            $photo = Photo::create(['file'=>$name]);
+            $inputs['photo_id'] = $photo->id;
+        }
+        $inputs['password'] = bcrypt($request->password);
+        User::create($inputs);
+        return redirect('admin/users');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
